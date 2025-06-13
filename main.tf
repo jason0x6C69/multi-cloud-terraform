dae783470af746ddc3c1e7c8b5dc428d772bd6ce
jason0x6C69 <36001958+jason0x6C69@users.noreply.github.com>
2025-06-13 16:55:12 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,53 +2,61 @@ terraform {
   required_providers {
     aws     = { source = "hashicorp/aws" }
     azurerm = { source = "hashicorp/azurerm" }
+    google  = { source = "hashicorp/google" }
     random  = { source = "hashicorp/random" }
   }
 }
 
 provider "aws" {
   region = "us-east-1"
-  alias  = "aws"
 }
 
 provider "azurerm" {
   features {}
-  alias = "azure"
-  resource_provider_registrations = "none"
+}
+
+provider "google" {
+  project = "multi-cloud-terraform-462716"
+  region  = "us-central1"
 }
 
 resource "random_id" "suffix" {
   byte_length = 4
 }
 
-# AWS: insecure S3 bucket
+# AWS: public-read S3 bucket (intentional misconfig)
 resource "aws_s3_bucket" "example" {
-  provider = aws
-  bucket   = "cloudsec-aws-${random_id.suffix.hex}"
-  acl      = "public-read"
+  bucket = "cloudsec-aws-${random_id.suffix.hex}"
+# acl    = "public-read"
 }
 
-# Azure: RG + storage container
+# Azure: resource group + insecure storage account
 resource "azurerm_resource_group" "example" {
-  provider = azurerm
   name     = "cloudsec-rg"
   location = "East US"
 }
 
 resource "azurerm_storage_account" "example" {
-  provider                          = azurerm
-  name                              = "cloudsec${random_id.suffix.hex}"
-  resource_group_name               = azurerm_resource_group.example.name
-  location                          = azurerm_resource_group.example.location
-  account_tier                      = "Standard"
-  account_replication_type          = "LRS"
-  allow_nested_items_to_be_public   = true
-  min_tls_version                   = "TLS1_0"
+  name                     = "cloudsec${random_id.suffix.hex}"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
 
+# Azure: public container (intentional misconfig)
 resource "azurerm_storage_container" "public" {
-  provider              = azurerm
   name                  = "public"
   storage_account_name  = azurerm_storage_account.example.name
   container_access_type = "blob"
+}
+
+# GCP: public-read GCS bucket (intentional misconfig)
+resource "google_storage_bucket" "example" {
+  name          = "cloudsec-gcp-${random_id.suffix.hex}"
+  location      = "US"
+  force_destroy = true
+
+  # grants allUsers READ access
+  # predefined_acl = "publicRead"
 }
